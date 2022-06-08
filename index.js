374a3e44ac5230f9b13e30792f548d6eebb21691
the4nna <the4nna@outlook.com>
2022-06-07 22:03:26 -0300

    adicionando formulário e stars em posts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
+const express = require('express') // os requires devem ser os primeiros elementos dentro do arquivo
 const path = require('path')
+const fs = require('fs')
+
 const app = express()
 
 // Definindo o template engine
@@ -16,6 +18,9 @@ const publicFolder = path.join(__dirname, 'public')
 const expressPublic = express.static(publicFolder)
 app.use(expressPublic)
 
+// habilita server para receber dados via post (formulário)
+app.use(express.urlencoded({ extended: true}))
+
 // rotas
 app.get('/', (req, res) => {
     res.render('index', {
@@ -23,9 +28,50 @@ app.get('/', (req, res) => {
     }) //sem o template engine: res.render('views/index ')
 })
 
+app.get('/cadastro-posts', (req, res) => {
+    const { c } = req.query
+    res.render('cadastro-posts', {
+        title: 'Digital Tech - Cadastrar Post',
+        cadastrado: c
+    }) //sem o template engine: res.render('views/index ')
+})
+
+app.post('/salvar-post', (req, res) => {
+    const {titulo, texto} = req.body
+
+    const data = fs.readFileSync('./store/posts.json')
+    const posts = JSON.parse(data)
+
+    posts.push({
+        titulo,
+        texto,
+    })
+
+    const postsString = JSON.stringify(posts)
+    fs.writeFileSync('./store/posts.json', postsString)
+
+    res.redirect('/cadastro-posts?c=1')
+})
+
 app.get('/posts', (req, res) => {
     res.render('posts', {
-        title: 'Digital Tech - Posts'
+        title: 'Digital Tech - Posts',
+        posts: [
+        {
+            title: 'Novidade no mundo da tecnologia',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut nunc venenatis, imperdiet velit non, feugiat turpis. Phasellus faucibus libero vitae luctus cursus. Morbi bibendum tincidunt accumsan. Quisque id luctus velit. Donec et sapien eleifend, aliquam purus non, mollis nulla. Fusce molestie nulla nunc, nec mattis augue ultricies ac.',
+            stars: 3
+        },
+        {
+            title: 'Criando um servidor com node.js',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut nunc venenatis, imperdiet velit non, feugiat turpis. Phasellus faucibus libero vitae luctus cursus. Morbi bibendum tincidunt accumsan. Quisque id luctus velit. Donec et sapien eleifend, aliquam purus non, mollis nulla. Fusce molestie nulla nunc, nec mattis augue ultricies ac.'
+        }, 
+        {
+            title: 'JS é a linguagem mais usada no mundo',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut nunc venenatis, imperdiet velit non, feugiat turpis. Phasellus faucibus libero vitae luctus cursus. Morbi bibendum tincidunt accumsan. Quisque id luctus velit. Donec et sapien eleifend, aliquam purus non, mollis nulla. Fusce molestie nulla nunc, nec mattis augue ultricies ac.',
+            stars: 5
+        }, 
+    ]
     })
 })
 
